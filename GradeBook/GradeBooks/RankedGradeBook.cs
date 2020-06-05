5729ec42fe57eeb36d1d3558475343b0980f4ac2
CJayP <cjpayne2704@gmail.com>
2020-06-05 22:14:31 +0100

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,6 +12,35 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            var studentsWithGrades = Students.Where(x => x.Grades.Count() > 0);
+            if (studentsWithGrades.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            if (studentsWithGrades.Count() >= 5)
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
 
