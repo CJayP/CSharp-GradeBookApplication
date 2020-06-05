26c61f29f79bc4a96de8e66bfcd4c3c73f945e1f
CJayP <cjpayne2704@gmail.com>
2020-06-05 22:07:10 +0100

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -24,25 +24,14 @@ namespace GradeBook.GradeBooks
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (averageGrade >= grades[threshold - 1])
-            {
                 return 'A';
-            }
-            else if (averageGrade >= grades[threshold * 2] - 1)
-            {
+            if (averageGrade >= grades[(threshold * 2) - 1])
                 return 'B';
-            }
-            else if (averageGrade >= grades[threshold * 3] - 1)
-            {
+            if (averageGrade >= grades[(threshold * 3) - 1])
                 return 'C';
-            }
-            else if (averageGrade >= grades[threshold * 4] - 1)
-            {
+            if (averageGrade >= grades[(threshold * 4) - 1])
                 return 'D';
-            }
-            else
-            {
-                return 'F';
-            }            
+            return 'F';
         }
     }
 }
